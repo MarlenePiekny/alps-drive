46ef63dae543eb14a25982dc2eba739eee903adb
MarlenePiekny <marlene.piekny@gmail.com>
2021-04-08 15:23:31 +0200

    feat: app working on http://localhost:3000/

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-var express = require('express');
+//Eléments à déclarer pour le serveur de l'appli qui se fait via express sur le port 3000
+const express = require('express');
+const app = express();
+const port = 3000;
+
+//Route vers le dossier frontend, par défaut le premier fichier est index.html
+app.use(express.static('frontend'));
+
+//Le projet s'ecoute sur le port 3000 du localhost
+app.listen(port, () => { })
