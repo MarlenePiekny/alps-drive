aa50ae3a9b9f4782ac3f6f157623f6812d26ee27
MarlenePiekny <marlene.piekny@gmail.com>
2021-04-26 12:06:08 +0200

    fix : Add 400 status to create a folder  if name is not alphanumeric

--- drive.js
+++ drive.js
@@ -1,6 +1,7 @@
 const fs = require('fs/promises');
 const path = require('path');
 
+//Déclaration de la route de base en dur
 const alpsDriveRoot = path.join('/tmp', 'base');
 
 //Créer un dossier de base
@@ -13,53 +14,36 @@ const createBaseFolder = () => {
     });
 };
 
-//Lister les fichiers à la racine du drive
-const listRootFolder = () => {
-    let promise = fs.readdir(alpsDriveRoot, { withFileTypes: true });
-    const transfPromise = promise
-        .then((result) => {
-            const tab = [];
-            result.map((element) => {
-                tab.push(
-                    {
-                        name: element.name,
-                        isFolder: element.isDirectory()
-                    }
-                )
-            });
-            return tab;
-        })
-        .catch(error => {
-            console.log('Erreur pour lister les fichiers et dossiers : ', error)
-        });
-    return transfPromise;
-};
-
-//Lister les fichiers selon leur nom
-const listFolder = (folder) => {
+//Lister les dossiers selon leur nom {name}
+const listFolder = (folder = '') => {
     let promise = fs.readdir(path.join(alpsDriveRoot, folder), { withFileTypes: true });
     const transfPromise = promise
         .then((result) => {
             const tab = [];
             result.map((element) => {
+                const promiseStat = fs.stat(path.join(alpsDriveRoot, folder, element.name));
+                const promiseFileStat = promiseStat.then((statFile) => {
+                    return statFile;
+                });
                 tab.push(
                     {
                         name: element.name,
+                        size: promiseFileStat.size,
                         isFolder: element.isDirectory()
                     }
                 )
             });
             return tab;
         })
-        .catch( (error) => {
+        .catch((error) => {
             console.log('Erreur pour lister les fichiers et dossiers : ', error)
         });
     return transfPromise;
 };
 
-//Lire le fichier selon son nom
+//Lire le fichier selon son nom {name}
 const readFile = (file) => {
-    const promise = fs.readFile(path.join(alpsDriveRoot,file), { encoding: "utf-8" });
+    const promise = fs.readFile(path.join(alpsDriveRoot, file), { encoding: "utf-8" });
     const transfPromise = promise
         .then((result) => {
             return result;
@@ -67,59 +51,70 @@ const readFile = (file) => {
     return transfPromise;
 };
 
-//Retourne le contenu de "name"
-const displayContent = (name) => {
+//Retourne le contenu de {name}
+const displayContent = (name = '') => {
     const promiseDisplay = fs.stat(path.join(alpsDriveRoot, name))
         .then((result) => {
             if (result.isDirectory()) {
                 //retourne le contenu du dossier
+                console.log(`Le dossier ${name} est lu`);
                 return listFolder(name);
             } else if (result.isFile()) {
-                //lire le fichier
+                //lit le fichier
+                console.log(`Le fichier ${name} est lu`);
                 return readFile(name);
             } else {
 
-            };   
+            };
         });
     return promiseDisplay;
 };
 
 //Crée un dossier avec le nom {name}
-const createFolder = (name, folder="") => {
-    const promiseCreatedFolder = fs.mkdir(path.join(alpsDriveRoot,folder, name))
-    .then( (result) => {
-        console.log(`Création dossier ${name}`);
-        return result;
-        })   
-    .catch( (error) => {
-        console.log("Erreur sur la fonction createFolder " + error);
-        });   
-    return promiseCreatedFolder;     
-};
+const createFolder = (name, folder = "") => {
+        const promiseCreatedFolder = fs.mkdir(path.join(alpsDriveRoot, folder, name))
+            .then((result) => {
+                console.log(`Création dossier ${name}`);
+                return result;
+            })
+            .catch((error) => {
+                console.log(`Le fichier ${name} n'a pas pu être créé : ${error}`);
+            });
+        return promiseCreatedFolder;
+    };
 
-//Vérifier si le nom du fichier est valide avec une RegEx
-const checkName = (name) => {
-    return /^[a-zA-Z1-9 ]*$/.test(name);
-}
+    //Vérifier si le nom du fichier est valide avec une RegEx
+    const isValid = (name) => {
+        return /^[a-zA-Z1-9 ]*$/.test(name);
+    }
 
-//Supprime un dossier ou un fichier avec le nom {name}
-const deleteContent = (name, folder="") => {
-    const promiseDeleteContent = fs.rm(path.join(alpsDriveRoot, folder, name), {force:true, recursive:true})
-    .then( (result) => {
-        console.log(`Le dossier ou fichier ${name} a été supprimé`);
-        return result; 
-    })
-    .catch( (error) => {
-        console.log("Erreur sur le fonction deleteContent : " + error);
-    })
-    return promiseDeleteContent;
-};
+    //Supprime un dossier ou un fichier avec le nom {name}
+    const deleteContent = (name, folder = "") => {
+        const promiseDeleteContent = fs.rm(path.join(alpsDriveRoot, folder, name), { recursive: true })
+        console.log(promiseDeleteContent);
+        return promiseDeleteContent;
+    };
 
-//Exporter chaque fonction
-module.exports = {
-    createFolder: createBaseFolder,
-    listRootFolder: listRootFolder,
-    displayContent: displayContent,
-    createFolder: createFolder,
-    deleteContent: deleteContent
-};
+
+    //Ajoute un fichier à la racine du drive
+    const addFile = (fileToUpload, folder = '') => {
+
+        //copie le fichier qui est créé temporairement
+        const promiseAddFile = fs.copyFile(fileToUpload.file, path.join(alpsDriveRoot, folder, fileToUpload.filename))
+
+        //supprime le fichier qui est créé temporairement
+        deleteContent(fileToUpload.uuid);
+        return promiseAddFile;
+
+    };
+
+    //Exporter chaque fonction
+    module.exports = {
+        createFolder: createBaseFolder,
+        displayContent: displayContent,
+        isValid: isValid,
+        createFolder: createFolder,
+        deleteContent: deleteContent,
+        addFile: addFile,
+        alpsDriveRoot: alpsDriveRoot,
+    };
--- server.js
+++ server.js
@@ -4,6 +4,14 @@ const drive = require('./drive');
 const express = require('express');
 const app = express();
 const port = 3000;
+const bb = require('express-busboy');
+
+//Permettre l'upload de fichiers
+bb.extend(app, {
+    upload: true,
+    path: drive.alpsDriveRoot,
+    allowedPath: /./
+});
 
 //Route vers le dossier frontend, par défaut le premier fichier est index.html
 app.use(express.static('frontend'));
@@ -13,31 +21,45 @@ app.listen(port, () => {
     console.log(`Alps Drive listening at http://localhost:${port}`);
 });
 
+/* ROUTES */
+
 //Retourne une liste contenant les dossiers et fichiers à la racine du “drive”
 app.get('/api/drive', (req, res) => {
-    drive.listRootFolder().then( (listFolder) => {
-       res.json(listFolder);
-    })
+    drive.displayContent()
+    .then( (listRootFolder) => {res.status(200).json(listRootFolder)})
 });
 
 //Retourne le contenu de {name} adapté en fonction de sa catégorie un dossier ou un fichier
 app.get('/api/drive/:name',(req, res) => {
     drive.displayContent(req.params.name)
-    .then( (content) => {res.send(content)})
+    .then( (listFolder) => {res.status(200).send(listFolder)})
     .catch( () => {res.status(404).send("Ce nom de dossier n'existe pas")})
 });
 
 //Crée un dossier avec le nom {name}
 app.post('/api/drive', (req, res) => {
+    if (drive.isValid(req.query.name)) {
+        drive.createFolder(req.query.name)
+        .then( () => {res.status(201).send()})
+    } else {
+        drive.createFolder(req.query.name)
+        .then( () => {res.status(400).send("Le nom doit être de type alphanumérique")})
+    }
     drive.createFolder(req.query.name)
-    .then( (createdFolder) => {res.send(createdFolder)})
-    .catch( () => { res.status(400).send("Le nom doit être de type alphanumérique")})
+    .catch( (err) => {res.status(500).send(err)});
 });
 
 //Crée un dossier avec le nom {name} dans {folder}
 app.post('/api/drive/:folder', (req, res) => {
+    if (drive.isValid(req.query.name)) {
+        drive.createFolder(req.query.name, req.params.folder)
+        .then( () => {res.status(201).send()})
+    } else {
+        drive.createFolder(req.query.name, req.params.folder)
+        .then( () => {res.status(400).send("Le nom doit être de type alphanumérique")})
+    }
     drive.createFolder(req.query.name, req.params.folder)
-    .then( (createdFolder ) => {res.send(createdFolder)})
+    .catch( (err) => {res.status(500).send(err)});
 });
 
 //Supprime un dossier ou un fichier avec le nom {name}
@@ -49,5 +71,18 @@ app.delete('/api/drive/:name', (req, res) => {
 //Supprime un dossier un fichier avec le nom {name} dans {folder}
 app.delete('/api/drive/:folder/:name', (req, res) => {
     drive.deleteContent(req.params.name, req.params.folder)
-    .then( () => {res.status(201).send()})
+    .then( (result) => { console.log('then', result); res.status(201).send()})
+    .catch( (result) => { console.log('catch', result); res.status(404).send('Le fichier n\'existe pas')})
+});
+
+//Crée un fichier à la racine du drive
+app.put('/api/drive', (req, res) => {
+    drive.addFile(req.files.file)
+    .then( (addedFile) => {res.status(201).send(addedFile) })
+});
+
+//Crée un fichier à la racine du drive
+app.put('/api/drive/:folder', (req, res) => {
+    drive.addFile(req.files.file, req.params.folder)
+    .then( (addedFile) => {res.status(201).send(addedFile) })
 });
